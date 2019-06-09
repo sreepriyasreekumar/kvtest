263a825ce88b821b42f3b4d03f32a8130fa07172
SreepriyaSreekumar <sreepriya.sreekumar@gds.ey.com>
2019-06-09 16:48:48 +0530

    fix: minor

--- index.js
+++ index.js
@@ -24,15 +24,16 @@ app.get('/token', async(req, res) => {
 app.get('/secret', async(req, res) => {
     try{
         let kvUrl = "https://block2vault.vault.azure.net/secrets/secret?api-version=2016-10-01";
-        let tokenData = await getToken();
-        console.log("Printing Token Data");
+        let tokenData = await getToken();   
+        console.log("Printing tokenData ---------------------------------------");
+        console.log("-------------------------------------------------------\n--------------------------------------------------");
         console.log(tokenData);
+        console.log("Done printing token Data");
+        console.log("-------------------------------------------------------\n--------------------------------------------------");
         let token = tokenData.access_token;
         console.log("Printing token");
         console.log(token);
         let secretData = await axios.get(kvUrl, {headers: {'Authorization': `Bearer ${token}`}});
-        console.log("Printing secret data");
-        console.log(secretData);
         res.status(200).send(secretData);
     }catch(err) {
         console.log("Error occurred");
