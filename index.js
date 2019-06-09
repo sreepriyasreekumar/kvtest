fab6cebff478dcc4631e9826951d7255993932d5
SreepriyaSreekumar <sreepriya.sreekumar@gds.ey.com>
2019-06-09 15:37:50 +0530

    fix: minor

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ app.listen(3000, () => {
 
 app.get('/testMe', async(req, res) => {
     try{
+        console.log("invoking get token");
         let result = await getToken();
+        console.log("Get token result: ");
         console.log(result);
         res.status(200).send(result);
 
     }catch(err) {
+        console.log("Error occurred");
         console.log(err);
         res.status(500).send(err);
     }
@@ -26,11 +29,13 @@ app.get('/testMe', async(req, res) => {
 
 const getToken = async () => {
     try{
+        console.log("Fetching the token")
         let tokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net";
         let tokenData = await axios.get(tokenUrl, {headers: {'Metadata': 'true'}});
         return tokenData;
 
     }catch(err) {
+        console.log("Error occurred");
         console.log(err);
     }
 }
